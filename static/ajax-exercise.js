f34caa46ce31cfdfacab39ac17b0c93113ac4cc8
Flora Ting <ting.flora@gmail.com>
2021-09-23 00:43:43 +0000

    Complete parts 1, 2, and some of 3; will edit $.post()

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,7 +3,11 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
+    evt.preventDefault();
   // TODO: get the fortune and show it in the #fortune-text div
+  $.get('/fortune', res => {
+      $('#fortune-text').html(res);
+  });
 }
 
 $('#get-fortune-button').on('click', showFortune);
@@ -15,8 +19,17 @@ function showWeather(evt) {
 
   const url = '/weather.json';
   const formData = {zipcode: $('#zipcode-field').val()};
+  // $.get(url, data, successFunction)
+  // the parameter data is an object; each key-value pair gets sent via query string
+  $.get(url, formData, res => {
+    // which of these is true?
+    // res = {'forecast': 'Kind of boring.', 'temp': '68F'}
+    // res = "{'forecast': 'Kind of boring.', 'temp': '68F'}"
 
+    $('#weather-info').text(res['forecast']);
+  });
   // TODO: request weather with that URL and show the forecast in #weather-info
+  
 }
 
 $('#weather-form').on('submit', showWeather);
@@ -26,6 +39,17 @@ $('#weather-form').on('submit', showWeather);
 function orderMelons(evt) {
   evt.preventDefault();
 
+  const url = '/order-melons.json';
+  const formInputs = {
+      qty: $('#qty-field').val(),
+      type: $('#melon-type-field').val()
+  };
+
+  $.post(url, formInputs, res => {
+      // res = jsonify({'code': result_code, 'msg': result_text}) <-- server.py
+      $('#order-status').html(res); // MAYBE FIXME
+      alert(res['qty'], res['type']); // FIXME
+  });
   // TODO: show the result message after your form
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
